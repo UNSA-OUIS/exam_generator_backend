21546904ece89b8de106f67e396b019f3d75a04c
RS7 <rsiza@unsa.edu.pe>
2025-07-08 11:52:25 -0500

    cors config for local

---
+++ config/cors.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'paths' => [
+        'sanctum/csrf-cookie',
+        'login',
+        'logout',
+        'api/*',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:5173',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
